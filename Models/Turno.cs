e0f136cb80ea65cd2194ac77178b991569eff350
roxana abigail vicentelo <abigailvicentelo@gmail.com>
2021-02-05 17:17:43 -0300

    modificación de la vista Informacion

--- Models/Turno.cs
+++ Models/Turno.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using AppMascota.Models;
 public class Turno{
     public Propietario propietario { get; set; }
     public Mascota mascota{ get; set; }
